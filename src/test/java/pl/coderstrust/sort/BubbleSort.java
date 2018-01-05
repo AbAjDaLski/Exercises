fad6bcd91b54532343180a602a2b647571ad608a
AbAjDaLski <bajdalski.a@protonmail.com>
2018-01-05 18:58:34 +0000

    fixed problem with Git.

---
+++ src/test/java/pl/coderstrust/sort/BubbleSort.java
@@ -0,0 +1,16 @@
+package pl.coderstrust.sort;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        System.out.println("BubbleSort");
+        Integer[] numbers = {18, 9, 1, 13, 12, 17};
+
+    }
+    public static Integer[] bubblesort(Integer[] numToSort) {
+        for (int i = 1; i < numToSort.length; i++) {
+
+            
+        }
+
+    }
+}
