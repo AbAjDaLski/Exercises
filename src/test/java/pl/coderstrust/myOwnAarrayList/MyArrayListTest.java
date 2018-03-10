b900be12f51d050535f23defdbcae5d99a1a4027
Your Name <you@example.com>
2018-03-10 11:27:08 +0100

    Cleaned up classes.

--- src/test/java/pl/coderstrust/myOwnAarrayList/MyArrayListTest.java
+++ src/test/java/pl/coderstrust/myOwnAarrayList/MyArrayListTest.java
@@ -1,5 +1,22 @@
 package pl.coderstrust.myOwnAarrayList;
 
-public class MyArrayListTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyArrayListTest {
+    private List<Long> givenList = new ArrayList<Long>();
+    private List<Long> expectedList = new ArrayList<Long>();
+
+    @Test
+    public void shouldToAddElemtntToList() {
+        //given
+        expectedList.add(1L);
+        //when
+        givenList.add(1L);
+        //then
+        Assert.assertEquals(expectedList.get(0), givenList.get(0));
+    }
 }
