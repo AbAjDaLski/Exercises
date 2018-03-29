33db98d3af585d6d94c70254a73c7ed8a41d4912
AbAjDaLski <you@example.com>
2018-03-29 20:26:15 +0200

    I've complited IPchecker with test.

--- src/main/java/pl/coderstrust/checkIpAddress/IP_check.java
+++ src/main/java/pl/coderstrust/checkIpAddress/IP_check.java
@@ -13,4 +13,5 @@ public class IP_check {
 
         return matcher.find();
     }
+
 }
--- src/test/java/pl/coderstrust/checkIpAddress/IP_checkTest.java
+++ src/test/java/pl/coderstrust/checkIpAddress/IP_checkTest.java
@@ -2,8 +2,7 @@ package pl.coderstrust.checkIpAddress;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
@@ -12,30 +11,64 @@ import java.util.List;
 @RunWith(JUnitParamsRunner.class)
 public class IP_checkTest {
 
+    static long timeBefore;
     IP_check ip_check = new IP_check();
+
+    @BeforeClass
+    public static void beforTest() {
+        timeBefore = System.currentTimeMillis();   //System.nanoTime();
+    }
 
     private List<String> ipProvider() {
         List<String> dataBaseIPv4 = new ArrayList<>();
-        for (int i = 0; i < 256; i++) {
-            for (int j = 0; j < 256; j++) {
-                for (int k = 0; k < 256; k++) {
-                    for (int l = 0; l < 256; l++) {
-                        dataBaseIPv4.add(Integer.toString(i) + "." + Integer.toString(j) + "." + Integer.toString(k) + "." + Integer.toString(l));
-                    }
-                }
+
+
+        String preFix = "1.1.1.";
+        String postFix = "";
+        for (int j = 0; j < 4; j++) {
+
+            for (int i = 0; i < 256; i++) {
+                dataBaseIPv4.add(preFix + Integer.toString(i) + postFix);
             }
+            if(preFix.length()>1) {
+                preFix = preFix.substring(0, preFix.length() - 2);
+            }else {
+                postFix="";
+            }
+            postFix = postFix.concat(".1");
         }
         return dataBaseIPv4;
+
     }
 
+    //@Ignore
     @Test
     @Parameters(method = "ipProvider")
-    public void isIpAddress(String input) {
+    public void isIpV4Address(String input) {
         //given
 
         //when
 
         //then
+
         Assert.assertTrue(ip_check.isIpAddress(input));
     }
+
+    @AfterClass
+    public static void afterTest() {
+        System.out.println("Operetion time = " + Long.toString(System.currentTimeMillis() - timeBefore) + "[milliseconds]");
+    }
+
+    @Test
+    @Parameters({"256.156.89.9", "123.-1.123.3", "300.2.0.5"})
+    public void isNotIpV4Address(String input) {
+        //given
+
+        //when
+
+        //then
+
+        Assert.assertFalse(ip_check.isIpAddress(input));
+    }
+
 }
